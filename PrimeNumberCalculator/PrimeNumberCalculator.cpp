fd78f2709100a4a960864dc748fa38330a171df8
Adam Beardow <abeardow13@gmail.com>
2021-03-28 22:37:57 +0100

    Updated to generate fibonacci numbers

--- PrimeNumberCalculator/PrimeNumberCalculator.cpp
+++ PrimeNumberCalculator/PrimeNumberCalculator.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <list>
+#include <vector>
 
 float maxNumber;
 int count = 0;
@@ -51,9 +53,7 @@ void calculatePrimeNumbers() {
 
 }
 
-int main()
-{
-    std::cout << "Prime Number Calculator\n";
+int primeNumberMachine() {
     std::cout << "Enter the maximum number:";
 
     if (validateNumber()) {
@@ -61,7 +61,7 @@ int main()
         calculatePrimeNumbers();
         auto stop = std::chrono::high_resolution_clock::now();
 
-        std::chrono::duration<double, std::milli> totalTime  = stop - start;
+        std::chrono::duration<double, std::milli> totalTime = stop - start;
 
         if (count == 0) {
             std::cout << "Found no prime numbers" << std::endl;
@@ -74,7 +74,7 @@ int main()
         else {
             std::cout << "Found " << count << " prime numbers" << std::endl;
         }
-        
+
         std::cout << "Program took " << totalTime.count() << "  milliseconds (" << totalTime.count() / 1000 << " seconds)" << std::endl;
         return 0;
     }
@@ -83,4 +83,64 @@ int main()
         std::cout << "Not a valid number";
         return -1;
     }
+}
+
+void calculateFibonacciNumbers() {
+    std::vector<unsigned long long int> numbers;
+    unsigned long long int i;
+
+    for (i = 0; i <= maxNumber; i++) {
+        if (i == 0) {
+            numbers.push_back(0);
+        }
+        else if (i == 1) {
+            numbers.push_back(1);
+        }
+        else {
+            numbers.push_back(numbers[i-1] + numbers[i-2]);            
+        }
+        std::cout << numbers[i] << std::endl;
+    }
+}
+
+int fibonacciMachine() {
+    std::cout << "Enter the maximum number:";
+
+    if (validateNumber()) {
+        auto start = std::chrono::high_resolution_clock::now();
+        calculateFibonacciNumbers();
+        auto stop = std::chrono::high_resolution_clock::now();
+
+        std::chrono::duration<double, std::milli> totalTime = stop - start;
+        std::cout << "Program took " << totalTime.count() << "  milliseconds (" << totalTime.count() / 1000 << " seconds)" << std::endl;
+        return 0;
+    }
+
+    else {
+        std::cout << "Not a valid number";
+        return -1;
+    }    
+}
+
+int main()
+{
+    int choice;
+    std::cout << "Algorithms in C++!\n";
+    std::cout << "Press 1 to calculate prime numbers or 2 to calculate fibonacci numbers:";
+
+    std::cin >> choice;
+
+    if (choice == 1) {
+        return primeNumberMachine();
+    }
+
+    else if (choice == 2) {
+        return fibonacciMachine();
+    }
+
+    else {
+        std::cout << "Not a valid choice!" << std::endl;
+        return 0;
+    }
+
 }
